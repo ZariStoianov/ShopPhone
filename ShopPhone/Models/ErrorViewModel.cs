7e177f0b0e37b5bfe95d065878252ef0b1596da8
Zari <zari_stoqnov@abv.bg>
2022-03-14 19:46:52 +0200

    Add project files.

---
+++ ShopPhone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace ShopPhone.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
